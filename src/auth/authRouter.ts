3302d5fd9725127ac15804f0dbc5f00fcdf2376c
Laponov Semen <johndoe@example.com>
2023-05-06 18:44:12 +0300

    fix regex mongo

--- src/auth/authRouter.ts
+++ src/auth/authRouter.ts
@@ -7,6 +7,9 @@ import {giveUserInformation, Login} from "./authDomain";
 export const authRouter = Router({})
 
 authRouter.post("/login",UserLoginOrEmailValidation, UserPasswordValidation, ValidationErrors, Login)
+authRouter.post("/registration",UserLoginOrEmailValidation, UserPasswordValidation, ValidationErrors, Login)
+authRouter.post("/registration-confirmation",UserLoginOrEmailValidation, UserPasswordValidation, ValidationErrors, Login)
+authRouter.post("/registration-email-resending",UserLoginOrEmailValidation, UserPasswordValidation, ValidationErrors, Login)
 authRouter.get("/me", giveUserInformation)
 
 const SECRET_KEY = 'My-secret-key'
