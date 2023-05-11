244010b38a8ac748f4791444a2d51a1cdc89fc7b
Laponov Semen <johndoe@example.com>
2023-05-11 12:11:49 +0300

    fix regex mongoreert

--- src/auth/authDomain.ts
+++ src/auth/authDomain.ts
@@ -12,35 +12,35 @@ import {
     createUnconfirmedUser, updateCodeOfUserConfirmation
 } from "../users/usersRepositoryMongoDB";
 import {v4 as uuidv4} from 'uuid'
-import {randomUUID} from "crypto";
-export async function Login(req: Request, res : Response) {
+
+export async function Login(req: Request, res: Response) {
     const loginOrEmail = req.body.loginOrEmail
-    const password =  req.body.password
+    const password = req.body.password
 
     const result = await LoginDB(loginOrEmail, password)
-    if(result){
+    if (result) {
         try {
             const JWT = await jwtService.createJWT(result)
             res.status(200).send({
                 accessToken: JWT
             })
             return
-        }catch(e){
+        } catch (e) {
             console.log(e)
         }
 
-    } else{
+    } else {
         console.log("no result found")
         res.sendStatus(401)
     }
 }
 
 
-export async function giveUserInformation(req: Request, res : Response) {
+export async function giveUserInformation(req: Request, res: Response) {
     const auth = req.headers.authorization
-    if(auth !== undefined) {
+    if (auth !== undefined) {
         const userId = await jwtService.getUserIdByToken(auth)
-        const userInfo = await usersCollectionOutput.findOne({_id : new ObjectId(userId!)})
+        const userInfo = await usersCollectionOutput.findOne({_id: new ObjectId(userId!)})
         res.status(200).send({
             "email": userInfo?.accountData.email,
             "login": userInfo?.accountData.login,
@@ -51,17 +51,17 @@ export async function giveUserInformation(req: Request, res : Response) {
     }
 }
 
-export async function sendMessageToEmail(req: Request, res : Response) {
+export async function sendMessageToEmail(req: Request, res: Response) {
     const login = req.body.login
     const password = req.body.password
     const email = req.body.email
 
     const userExists = await checkUserExistance(login, password, email)
-    if(userExists){
+    if (userExists) {
         res.status(400).send(userExists)
     } else {
         const user = await createUnconfirmedUser(login, password, email)
-        if(user) {
+        if (user) {
             await emailAdapter.sendEmail(req.body.email, user.accountConfirmationData.code)
             res.sendStatus(204)
         } else {
@@ -69,39 +69,41 @@ export async function sendMessageToEmail(req: Request, res : Response) {
         }
     }
 }
-export async function registrationConfirmation(req: Request, res : Response) {
-    const code : string = req.body.code
-    if(code){
-        const codeVerificationResult = await codeVerification(code.toString())
-        if(!codeVerificationResult){
-            res.status(400).send({errorsMessages : [{"message": "wrong code passed228", "field": "code"}]})
-        } else {
-            if(await confirmUserStatus(code.toString())){
-                res.sendStatus(204)
-            }
-            res.status(400).send({errorsMessages : [{"message": "no code in query params passed", "field": "code"}]})
+
+export async function registrationConfirmation(req: Request, res: Response) {
+    const code: string = req.body.code
+    const codeVerificationResult = await codeVerification(code.toString())
+    if (!codeVerificationResult) {
+        res.status(400).send({errorsMessages: [{"message": "wrong code passed228", "field": "code"}]})
+    } else {
+        if (await confirmUserStatus(code.toString())) {
+            res.sendStatus(204)
+            return
         }
-
-
-    }else{
-        res.status(400).send({errorsMessages : [{"message": "no code in query params passed", "field": "code"}]})
+        res.status(400).send({errorsMessages: [{"message": "no code in query params passed", "field": "code"}]})
+        return
     }
 
 }
-export async function registrationEmailResending(req: Request, res : Response) {
+
+export async function registrationEmailResending(req: Request, res: Response) {
     const email = req.body.email
     const userExists = await checkUserExistanceByEmail(email)
-    if(!userExists){
-        res.status(400).send({"errorsMessages": [{
-                    "message": "user with such email doesnt exists",
-                    "field": "email"
-                }] })
-    } else{
-        if(await checkingForUserConfirmationStatus(email)){
-            res.status(400).send({"errorsMessages": [{
+    if (!userExists) {
+        res.status(400).send({
+            "errorsMessages": [{
+                "message": "user with such email doesnt exists",
+                "field": "email"
+            }]
+        })
+    } else {
+        if (await checkingForUserConfirmationStatus(email)) {
+            res.status(400).send({
+                "errorsMessages": [{
                     "message": "user already confirmed",
                     "field": "isConfirmed"
-                }] })
+                }]
+            })
             return
         }
 
